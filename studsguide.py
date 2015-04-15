32b950d27fd7b6b38b8196d4fa2c4f02cab4bf96
Ludvig Jansson <ludjan@kth.se>
2015-04-15 15:55:18 +0200

    yolo

--- studsguide.py
+++ studsguide.py
@@ -4,13 +4,16 @@ Highrise.set_server('froxy')
 Highrise.auth('f4075d9e048c00bda63ec55794d02831')
 
 deal_ids = []
+new_deals = []
 s = sched.scheduler(time.time, time.sleep)
 def swag(sc):
 	print "Polling..."
 	deals = Deal.all()
+	new_deals = []
 	for deal in deals:
 		if not deal.id in deal_ids:
 			deal_ids.append(deal.id)
+			new_deals.append(deal)
 			print "%s" % deal.name
 	sc.enter(5, 1, swag, (sc,))
 
