abed427ba63ef205f542bcee307c5a0eb6c93e8b
Ludvig Jansson <ludjan@kth.se>
2015-04-15 15:51:44 +0200

    yolo

--- studsguide.py
+++ studsguide.py
@@ -1,8 +1,19 @@
 from pyrise import *
-
+import sched, time
 Highrise.set_server('froxy')
 Highrise.auth('f4075d9e048c00bda63ec55794d02831')
 
-deal = Deal.all()
-for deal in deal:
-	print "%s" % deal.name
+deal_ids = []
+s = sched.scheduler(time.time, time.sleep)
+def swag(sc):
+	print "Polling..."
+	deals = Deal.all()
+	for deal in deals:
+		if not deal.id in deal_ids:
+			deal_ids.append(deal.id)
+			print "%s" % deal.name
+	sc.enter(5, 1, swag, (sc,))
+
+s.enter(5, 1, swag, (s,))
+s.run()
+
