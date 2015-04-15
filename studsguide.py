4e3a5acaac47233cee64c57ec5f03ed9478cad1d
Ludvig Jansson <ludjan@kth.se>
2015-04-15 16:05:26 +0200

    yolo

--- studsguide.py
+++ studsguide.py
@@ -6,8 +6,11 @@ Highrise.auth('f4075d9e048c00bda63ec55794d02831')
 deal_ids = []
 new_deals = []
 s = sched.scheduler(time.time, time.sleep)
+counter = 0
+passive_aggressive = 0
 def swag(sc):
-	print "Polling..."
+	msg = "Polling..."
+	counter += 1
 	deals = Deal.all()
 	new_deals = []
 	for deal in deals:
@@ -15,6 +18,25 @@ def swag(sc):
 			deal_ids.append(deal.id)
 			new_deals.append(deal)
 			print "%s" % deal.name
+	if counter > 5:
+		if passive_aggressive == 0:
+			msg = "Getting bored here..."
+			passive_aggressive = 1
+			counter = 0
+		elif passive_aggressive == 2:
+			msg = "Nothing is happening!"
+			passive_aggressive = 2
+			counter = 0
+		elif passive_aggressive == 3:
+			msg = "Why am I still doing this?"
+			passive_aggressive = 3
+			counter = 0
+		elif passive_aggressive == 4:
+			msg = "What is my purpose? What is life?"
+			passive_aggressive = 0
+			counter = 0 
+	print msg
+
 	sc.enter(5, 1, swag, (sc,))
 
 s.enter(5, 1, swag, (s,))
